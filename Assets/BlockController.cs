77f238a8ddfdc89a8d56ac2ff9df210404711bcd
EKT <efekagantelevi@gmail.com>
2023-05-24 17:00:05 +0300

    Prefabs-GameManager

---
+++ Assets/BlockController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    private void Start()
+    {
+
+        StartCoroutine(routine: MoveDown());
+
+    }
+
+    IEnumerator MoveDown()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            var position = transform.position;
+            position.y--;
+            transform.position = position;
+        }
+        
+    }
+}
